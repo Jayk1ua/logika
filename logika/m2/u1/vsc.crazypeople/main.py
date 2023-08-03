71cabe63f8e97e106dc07f334e67238b4528ab05
Jayk1ua <140438422+Jayk1ua@users.noreply.github.com>
2023-08-03 19:03:11 +0300

    гітлер

--- logika/m2/u1/vsc.crazypeople/main.py
+++ logika/m2/u1/vsc.crazypeople/main.py
@@ -1,8 +1,12 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
 
 app = QApplication([])
 main_window = QWidget()
+
+button = QPushButton('Натисни!')
+text = QLabel('Натисни . . . .')
+winner = QLabel('?')
 
 main_window.show()
 app.exec_()
