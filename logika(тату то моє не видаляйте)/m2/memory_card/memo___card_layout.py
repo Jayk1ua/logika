45acadacfda1ea0dafa3652b6ec3c94a2ada955a
Jayk1ua <140438422+Jayk1ua@users.noreply.github.com>
2023-08-31 19:57:44 +0300

    Add files via upload

--- logika(тату то моє не видаляйте)/m2/memory_card/memo___card_layout.py
+++ logika(тату то моє не видаляйте)/m2/memory_card/memo___card_layout.py
@@ -1,92 +1,132 @@
 ''' Вікно для картки питання '''
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-        QApplication, QWidget, 
-        QTableWidget, QListWidget, QListWidgetItem,
-        QLineEdit, QFormLayout,
-        QHBoxLayout, QVBoxLayout, 
-        QGroupBox, QButtonGroup, QRadioButton,  
-        QPushButton, QLabel, QSpinBox)
+    QApplication, QWidget,
+    QTableWidget, QListWidget, QListWidgetItem,
+    QLineEdit, QFormLayout,
+    QHBoxLayout, QVBoxLayout,
+    QGroupBox, QButtonGroup, QRadioButton,
+    QPushButton, QLabel, QSpinBox)
+
+
 app = QApplication([])
 
 
-btn_menu = QPushButton('Меню')
+# віджети, які треба буде розмістити:
+btn_Menu = QPushButton('Меню')  # кнопка повернення до основного вікна
+# кнопка прибирає вікно та повертає його після закінчення таймера
+btn_Sleep = QPushButton('Відпочити')
+box_Minutes = QSpinBox()  # введення кількості хвилин
+box_Minutes.setValue(30)
+btn_OK = QPushButton('Відповісти')  # кнопка відповіді
+lb_Question = QLabel('')  # текст питання
 
-btn_sleep = QPushButton('Відпочити')
-btn_ok = QPushButton('Відповісти')
-lb_question = QLabel('')
 
-box_minutes = QSpinBox()
-box_minutes.setValue(5)
-
-RadioGroupBox = QGroupBox('Варіанти відповідей')
+# Панель з варіантами:
+# група на екрані для перемикачів із відповідями
+RadioGroupBox = QGroupBox("Варіанти відповіді")
+# а це для угруповання перемикачів, щоб керувати їхньою поведінкою
 RadioGroup = QButtonGroup()
-rbtn1 = QRadioButton('')
-rbtn2 = QRadioButton('')
-rbtn3 = QRadioButton('')
-rbtn4 = QRadioButton('')
 
-RadioGroup.addButton(rbtn1)
-RadioGroup.addButton(rbtn2)
-RadioGroup.addButton(rbtn3)
-RadioGroup.addButton(rbtn4)
 
+rbtn_1 = QRadioButton('')
+rbtn_2 = QRadioButton('')
+rbtn_3 = QRadioButton('')
+rbtn_4 = QRadioButton('')
+
+
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+
+# Панель із результатом:
+AnsGroupBox = QGroupBox("Результат тесту")
+lb_Result = QLabel('')  # тут розміщується напис "правильно" або "неправильно"
+lb_Correct = QLabel('')  # тут буде написано текст правильної відповіді
+
+
+# Тепер займаємося розміщенням:
+
+
+# Розміщуємо варіанти відповідей у два стовпці всередині групи:
 layout_ans1 = QHBoxLayout()
-layout_ans2 = QVBoxLayout()
+layout_ans2 = QVBoxLayout()  # вертикальні будуть усередині горизонтального
 layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1)  # дві відповіді у перший стовпець
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3)  # дві відповіді у другий стовпець
+layout_ans3.addWidget(rbtn_4)
 
-layout_ans2.addWidget(rbtn1)
-layout_ans2.addWidget(rbtn2)
-layout_ans3.addWidget(rbtn3)
-layout_ans3.addWidget(rbtn4)
 
 layout_ans1.addLayout(layout_ans2)
-layout_ans1.addLayout(layout_ans3)
+layout_ans1.addLayout(layout_ans3)  # розмістили стовпці в одному рядку
 
-RadioGroupBox.setLayout(layout_ans1)
 
-AnsGroupbox = QGroupBox('Результат тесту')
-lb_result = QLabel('')
-lb_correct = QLabel('')
-
+RadioGroupBox.setLayout(layout_ans1)  # готова "панель" з варіантами відповідей
+# розміщуємо результат:
 layout_res = QVBoxLayout()
-layout_res.addWidget(lb_result, alignment=(Qt.AlignLeft | Qt.AlignTop))
-layout_res.addWidget(lb_correct, alignment=Qt.AlignHCenter)
-AnsGroupbox.setLayout(layout_res)
-AnsGroupbox.hide()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+AnsGroupBox.hide()
 
-Layout_line1 = QHBoxLayout()
-Layout_line2 = QHBoxLayout()
-Layout_line3 = QHBoxLayout()
-Layout_line4 = QHBoxLayout()
 
-Layout_line1.addWidget(btn_menu)
-Layout_line1.addStretch(1)
-Layout_line1.addWidget(btn_sleep)
-Layout_line1.addWidget(box_minutes)
-Layout_line1.addWidget(QLabel('Хвилин'))
+# розміщуємо всі віджети у вікні, вони розташовані в чотири рядки:
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+layout_line4 = QHBoxLayout()
 
-Layout_line2.addWidget(lb_question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
 
-Layout_line3.addWidget(RadioGroupBox)
-Layout_line3.addWidget(AnsGroupbox)
+layout_line1.addWidget(btn_Menu)
+layout_line1.addStretch(1)  # розрив між кнопками робимо по можливості довшим
+layout_line1.addWidget(btn_Sleep)
+layout_line1.addWidget(box_Minutes)
+# нам не потрібна змінна для цього напису
+layout_line1.addWidget(QLabel('хвилин'))
 
-Layout_line4.addStretch(1)
-Layout_line4.addWidget(btn_ok)
-Layout_line4.addStretch(1)
 
+layout_line2.addWidget(lb_Question, alignment=(
+    Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line3.addWidget(RadioGroupBox)
+layout_line3.addWidget(AnsGroupBox)
+layout_line4.addStretch(1)
+layout_line4.addWidget(btn_OK, stretch=2)  # кнопка має бути великою
+layout_line4.addStretch(1)
+
+
+# Тепер створені 4 рядки розмістимо один під одним:
 layout_card = QVBoxLayout()
-layout_card.addLayout(Layout_line1)
-layout_card.addLayout(Layout_line2)
-layout_card.addLayout(Layout_line3)
-layout_card.addLayout(Layout_line4)
-
+layout_card.addLayout(layout_line1, stretch=1)
+layout_card.addLayout(layout_line2, stretch=2)
+layout_card.addLayout(layout_line3, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line4, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5)  # прогалини між вмістом
+# Результат роботи цього модуля: віджети розміщені всередині layout_card, який можна призначити вікну.
 
 
 def show_result():
     ''' показати панель відповідей '''
-    pass
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Наступне питання')
+
 
 def show_question():
-    ''' показати панель запитань '''
-    pass
+    ''' показати панель питань '''
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Відповісти')
+    # скинути вибрану радіо-кнопку
+    # зняли обмеження, щоб можна було скинути вибір радіокнопки
+    RadioGroup.setExclusive(False)
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    # повернули обмеження, тепер лише одна радіокнопка може бути вибрана
+    RadioGroup.setExclusive(True)
