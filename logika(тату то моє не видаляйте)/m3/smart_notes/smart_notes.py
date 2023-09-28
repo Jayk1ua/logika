9fda6366ab3d0217923d3cb604d90eaa2776229b
Jayk1ua <140438422+Jayk1ua@users.noreply.github.com>
2023-09-28 20:01:41 +0300

    Add files via upload

--- logika(тату то моє не видаляйте)/m3/smart_notes/smart_notes.py
+++ logika(тату то моє не видаляйте)/m3/smart_notes/smart_notes.py
@@ -7,9 +7,16 @@ from PyQt5.QtWidgets import (
     QFormLayout, QGroupBox, QButtonGroup, QRadioButton, QSpinBox)
 import json 
 
+def writeToFile():
+    with open('notes.json', 'w', encoding='utf-8') as file:
+        json.dump(notes, file, ensure_ascii=False, sort_keys=True, indent=4)
+
+
 #Вікно
 app = QApplication([])
 window = QWidget()
+
+
 
 #Кнопки
 field_text = QTextEdit()
@@ -31,13 +38,13 @@ btn_tag_search = QPushButton('Шукати за тегом')
 btn_tag_searchfor = QLineEdit()
 
 
-#Лінії + віджети
+#Лінії + додавання віджетів
 layout_notes = QHBoxLayout()
 col1 = QVBoxLayout()
 col2 = QVBoxLayout()
 
-layout_notes.addLayout(col1)
-layout_notes.addLayout(col2)
+layout_notes.addLayout(col1, stretch=2)
+layout_notes.addLayout(col2, stretch=1)
 
 col1.addWidget(field_text)
 
@@ -67,11 +74,57 @@ window.setLayout(layout_notes)
 
 
 
-
-
+#Відкриття файлу в json
 with open('notes.json', 'r', encoding='utf-8') as file:
     notes = json.load(file)
 
+
+
+#функція, щоб дістати текст
+def show_note():
+    key = lst_notes.currentItem().text()
+
+    field_text.setText(notes[key]['текст'])
+
+    lst_tags.clear()
+    lst_tags.addItems(notes[key]['теги'])
+
+
+#Програмування кнопок
+def add_note():
+    note_name, ok = QInputDialog.getText(window, 'Додати замітку', 'Назва замітки')
+
+    if note_name and ok:
+        notes[note_name] = {'текст': '', 'теги': []}
+        lst_notes.addItem(note_name)
+
+
+def save_note():
+    if lst_notes.currentItem():
+        key = lst_notes.currentItem().text()
+        notes[key]['текст'] = field_text.toPlainText()
+        writeToFile()
+
+
+def del_note():
+    if lst_notes.currentItem():
+        key  = lst_notes.currentItem().text()
+        del notes[key]
+
+        field_text.clear()
+        lst_tags.clear()
+        lst_notes.clear()
+
+        lst_notes.additems(notes)
+        writeToFile()
+
+
+#!!!
+lst_notes.itemClicked.connect(show_note)
+
+btn_add_notes.clicked.connect(add_note)
+btn_save_notes.clicked.connect(save_note)
+btn_del_notes.clicked.connect(del_note)
 lst_notes.addItems(notes)
 
 #Показ
